3214b7d01d2ef3711a0d1b5638cf5efb2b4927d5
Jitendra Josyula <jitendra.jjr@gmail.com>
2024-02-01 14:09:24 -0500

    added dynamic creds

--- index.js
+++ index.js
@@ -5,18 +5,24 @@ const port = 3000
 var oauth2 = new jsforce.OAuth2({
     // you can change loginUrl to connect to sandbox or prerelease env.
     // loginUrl : 'https://test.salesforce.com',
-    clientId : '3MVG9ux34Ig8G5eoKHMdUtmhw6L948uDpNZEX.tnOTl3jrO3L2vUMPuLFXu9.G.t9rRSIYDjVnr8Pj_RzmniX',
-    clientSecret : '485FD9A8F14DC6AC21BF514E6469FD7050D5F83655ABDD170BADF534D7C1F619',
-    redirectUri : 'https://rtmsdevorg-dev-ed.my.salesforce-sites.com/'
+    // clientId : '3MVG9ux34Ig8G5eoKHMdUtmhw6L948uDpNZEX.tnOTl3jrO3L2vUMPuLFXu9.G.t9rRSIYDjVnr8Pj_RzmniX',
+    // clientSecret : '485FD9A8F14DC6AC21BF514E6469FD7050D5F83655ABDD170BADF534D7C1F619',
+    // redirectUri : 'https://rtmsdevorg-dev-ed.my.salesforce-sites.com/'
+    clientId : 'clientId',
+    clientSecret : 'clientSecret',
+    redirectUri : 'redirectUri'
   });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 app.get('/oauth2/auth', function(req, res) {
-    console.log('url: ' + req.query.domain);
-    let urlStr =oauth2.getAuthorizationUrl({ scope : 'full', prompt :'login' });
-    console.log('urlStr:::',urlStr)
+    console.log('domain: ' + req.query.domain);
+    console.log('clientId: ' + req.query.clientId);
+    console.log('redirecturl: ' + req.query.redirecturl);
+    //let urlStr =oauth2.getAuthorizationUrl({ scope : 'full', prompt :'login'});
+     let urlStr =oauth2.getAuthorizationUrl({ scope : 'full', prompt :'login','client_id' : req.query.clientId,'redirect_uri': req.query.redirecturl});
+     console.log('urlStr:::',urlStr)
     let finalUrlStr = req.query.domain+'/services'+urlStr.split('/services')[1];
     console.log('uuu:::',finalUrlStr)
     res.redirect(finalUrlStr);
