505d237ba1e87eb2c09c734d416a5973bd92d8f6
Jitendra Josyula <jitendra.jjr@gmail.com>
2024-02-01 11:35:32 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+var jsforce = require('jsforce');
+const app = express()
+const port = 3000
+var oauth2 = new jsforce.OAuth2({
+    // you can change loginUrl to connect to sandbox or prerelease env.
+    // loginUrl : 'https://test.salesforce.com',
+    clientId : '3MVG9ux34Ig8G5eoKHMdUtmhw6L948uDpNZEX.tnOTl3jrO3L2vUMPuLFXu9.G.t9rRSIYDjVnr8Pj_RzmniX',
+    clientSecret : '485FD9A8F14DC6AC21BF514E6469FD7050D5F83655ABDD170BADF534D7C1F619',
+    redirectUri : 'https://rtmsdevorg-dev-ed.my.salesforce-sites.com/'
+  });
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+app.get('/oauth2/auth', function(req, res) {
+    console.log('uuu:::',oauth2.getAuthorizationUrl({ scope : 'full' }))
+    res.redirect(oauth2.getAuthorizationUrl({ scope : 'full' , prompt :'login'}));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
